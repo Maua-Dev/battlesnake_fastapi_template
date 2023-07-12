4400b0f2d4ab2d1e939c996f9bc7c640ae2cf090
Vitor Soller <vgsoller@gmail.com>
2023-07-12 14:04:34 -0300

    add workflows 👽

--- iac/app.py
+++ iac/app.py
@@ -7,6 +7,21 @@ from iac.iac_stack import IacStack
 
 
 app = cdk.App()
-IacStack(app, "IacStack")
+
+aws_region = os.environ.get("AWS_REGION")
+aws_account_id = os.environ.get("AWS_ACCOUNT_ID")
+stack_name = os.environ.get("STACK_NAME")
+github_ref_name = os.environ.get("GITHUB_REF_NAME")
+project_name = os.environ.get("PROJECT_NAME")
+
+
+tags = {
+    'project': project_name,
+    'stage': "TEST",
+    'stack': 'BACK',
+    'owner': 'DevCommunity'
+}
+
+IacStack(app, project_name, env=cdk.Environment(account=aws_account_id, region=aws_region), tags=tags)
 
 app.synth()
