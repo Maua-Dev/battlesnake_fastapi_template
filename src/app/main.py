0d9c18a4ea06a8408001a4fbf9e919abf9bcfff2
Vitor Soller <vgsoller@gmail.com>
2023-07-12 18:56:00 -0300

    remove/

--- src/app/main.py
+++ src/app/main.py
@@ -13,8 +13,7 @@ def read_root():
 def read_item(item_id: int):
     return {"item_id": item_id}
 
-@app.post("/create_item/")
-@app.post("")
+@app.post("/create_item")
 def create_item(request: dict):
     item_id = request.get("item_id")
     name = request.get("name")
